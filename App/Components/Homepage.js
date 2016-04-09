a3dce0d3e13f2b52a831a469350fc9f20ed5e9ce
penguyen <peternguyen008@gmail.com>
2016-04-09 18:10:05 -0400

    implementing ScrollView and Refreshable Control to Homepage

--- App/Components/Homepage.js
+++ App/Components/Homepage.js
@@ -1,20 +1,33 @@
 'use strict'
 const React = require('react-native');
 const Firebase = require('firebase');
-var styles = require('./Helpers/Styles');
+// var styles = require('./Helpers/Styles');
 var ViewSite = require('./Helpers/Web');        // WebView     
 var ref = new Firebase('https://dazzling-inferno-3629.firebaseio.com');
 var Search = require('./Search');               // for testing -- belongs in Nav
 // var Nav = require('./Nav');
 
 var {
+  Dimensions,
+  Navigator,
+  PixelRatio,
+  RefreshControl,
+  ScrollView,
+  StyleSheet,
   Text,
   TouchableHighlight,
-  StyleSheet,
-  View,
-  Navigator,
-  ListView
+  TouchableWithoutFeedback,
+  View
 } = React;
+
+
+// Scrollable Parallax View
+var Parallax = require('react-native-parallax');
+var IMAGE_WIDTH = Dimensions.get('window').width;
+var IMAGE_HEIGHT = IMAGE_WIDTH / 2;
+var PIXEL_RATIO = PixelRatio.get();
+var PARALLAX_FACTOR = 0.3;
+
 
 // verifies user auth state 
 function authDataCallback(authData) {
@@ -37,7 +50,18 @@ class Homepage extends React.Component{
   // called ONCE BEFORE Homepage Component is rendered
   componentWillMount(){
     this.getFavorites();        // getting favorites (in case of new additions)
+    console.log('Dimensions IMAGE_WIDTH: ', IMAGE_WIDTH)
+    console.log('Dimensions IMAGE_HEIGHT: ', IMAGE_HEIGHT)
+    console.log('Dimensions PIXEL_RATIO: ', PIXEL_RATIO)
   }
+
+  // called ONCE AFTER Homepage Component is rendered
+  componentDidMount(){
+    console.log('AFTERWARDS')
+    this.getFavorites();        // getting favorites (in case of new additions)
+  }
+
+
 
   // Queries & setState of apartment favorites from Firebase
   getFavorites(){
@@ -107,35 +131,90 @@ class Homepage extends React.Component{
     }
 
     return(
-      <View style={styles.mainContainer}>
-        {/* Homepage Greeting Header */}
-        <Text style={styles.header}>Hello, {this.props.user.name}!</Text>
+      <ScrollView style={styles.scrollView}>
+
         {/* ListView of User's Favorited Apartments */}
-        <View style={styles.favorites}>
-          <Text style={styles.listTitle}>Favorites List</Text>
-          <Text style={styles.listFavs}>List of Favorite Apartments here</Text>
-
-          {/* Temp 'Search' Button to Search Component */}
-          <TouchableHighlight
-            style={styles.button}
-            onPress={this.handleGoToSearch.bind(this)}
-            underlayColor='white' >
-            <Text style={styles.buttonText}>Find Apartments</Text>
-          </TouchableHighlight>
-
-          {/* Temp Logout Button */}
-          <TouchableHighlight
-            style={styles.button}
-            onPress={this.handleLogout.bind(this)}
-            underlayColor='white' >
-            <Text style={styles.buttonText}>Logout</Text>
-          </TouchableHighlight>
+        <View style={styles.overlay}>
+          <Text style={styles.title}>Favorites List</Text>
 
         </View>
-      </View>
+
+        {/* Temp 'Search' Button to Search Component */}
+        <TouchableHighlight
+          style={styles.button}
+          onPress={this.handleGoToSearch.bind(this)}
+          underlayColor='white' >
+          <Text style={styles.buttonText}>Find Apartments</Text>
+        </TouchableHighlight>
+
+        {/* Temp Logout Button */}
+        <TouchableHighlight
+          style={styles.button}
+          onPress={this.handleLogout.bind(this)}
+          underlayColor='white' >
+          <Text style={styles.buttonText}>Logout</Text>
+        </TouchableHighlight>
+      </ScrollView>
     )
   }
 }
+
+
+// Homepage StyleSheet
+var styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    padding: 30,
+    marginTop: 65,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    backgroundColor: '#48BBEC'
+  },
+  image: {
+    height: IMAGE_HEIGHT,
+  },
+  overlay: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0,0,0,0.3)',
+  },
+  title: {
+    fontSize: 20,
+    textAlign: 'center',
+    lineHeight: 25,
+    fontWeight: 'bold',
+    color: 'white',
+    shadowOffset: {
+      width: 0,
+      height: 0,
+    },
+    shadowRadius: 1,
+    shadowColor: 'black',
+    shadowOpacity: 0.8,
+  },
+  url: {
+    opacity: 0.5,
+    fontSize: 10,
+    position: 'absolute',
+    color: 'white',
+    left: 5,
+    bottom: 5,
+  },
+  row: {
+    borderColor: 'grey',
+    borderWidth: 1,
+    padding: 20,
+    backgroundColor: '#3a5795',
+    margin: 5,
+  },
+  text: {
+    alignSelf: 'center',
+    color: '#fff',
+  },
+  scrollview: {
+    flex: 1,
+  },
+});
 
 Homepage.propTypes = {
   user: React.PropTypes.object.isRequired,
