3710b30f5d6d5eb08c6661ef58fa91715c0f92ed
penguyen <peternguyen008@gmail.com>
2016-05-10 21:27:12 -0400

    created first draft of Signup component

--- components/Intro.js
+++ components/Intro.js
@@ -1,1 +1,32 @@
-Intro.js
+'use strict'
+const React = require('react-native');
+var { Stylesheet, Text, TouchableHighlight, View } = React;
+const Button = require('react-native-button');
+
+// Initial component page the user will see 
+class Intro extends React.Component {
+  render() {
+    var Actions = this.props.routes;      // console.log this.props.routes (what data/routes is inside?)
+    
+    return (
+      <View style={styles.container}>
+        <Text>Welcome to Padticular!</Text>
+        <Text>Discovering beautiful rental abodes with the swipe of a finger</Text>
+        <Button onPress={ ()=>Actions.login({ data:"Custom data", title:"Custom title" })}> Login </Button>
+        <Button onPress={Actions.signup}> New Member? </Button>
+      </View>
+    )
+  }
+}
+
+// Styling for Intro component page
+var styles = Stylesheet.create({
+  container: {
+    flex: 1, 
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'transparent',
+  }
+});
+
+module.exports = Intro;
--- components/Signup.js
+++ components/Signup.js
@@ -1,1 +1,52 @@
-Signup.js
+'use strict'
+const React = require('react-native');
+var { Stylesheet, Text, View } = React;
+const Button = require('react-native-button');
+
+// Database connection to Firebase
+const Firebase = require('firebase');
+const userRef = new Firebase('https://dazzling-inferno-3629.firebaseio.com');
+const user = userRef.child('/users'); 
+
+// Signup component - registration form for new members
+class Signup extends React.Component {
+  
+  // need methods to create & verify new member info in Firebase!
+  // need to create Registration form with TextInput fields for: Full Name, Email & Create Password!
+
+  render() {
+    let Actions = this.props.routes;      // what info is inside this.props.routes??
+
+    return (
+      <View style={styles.container}>
+        <Text>New Member Registration</Text>
+        <Button onPress={Actions.login}>Sign me up!</Button>
+        <Button onPress={Actions.pop}>Back</Button>
+      </View>
+    )
+  }
+}
+
+// Styling for Intro component page
+var styles = Stylesheet.create({
+  container: {
+    flex: 1, 
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+module.exports = Signup;
+
+
