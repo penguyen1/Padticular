f3628a8b6d89e8171155796727484fe8a46d2a63
penguyen <peternguyen008@gmail.com>
2016-05-11 01:43:07 -0400

    implementing new syntax for react-native v0.25.1

--- components/Intro.js
+++ components/Intro.js
@@ -1,32 +1,48 @@
 'use strict'
-var React = require('react-native');
-var { Stylesheet, Text, TouchableHighlight, View } = React;
+// var React = require('react-native');
+// var { Stylesheet, Text, TouchableHighlight, View } = React;
+import React, {Component} from 'react';
+import { Stylesheet, Text, TouchableHighlight, View } from 'react-native';
 const Button = require('react-native-button');
 
 // Initial component page the user will see 
-class Intro extends React.Component {
+class Intro extends Component {
   render() {
     var Actions = this.props.routes;      // console.log this.props.routes (what data/routes is inside?)
     
     return (
-      <View style={styles.container}>
+      <View style={{ flex:1, justifyContent:'center', alignItems:'center', backgroundColor:'transparent' }}>
         <Text>Welcome to Padticular!</Text>
         <Text>Discovering beautiful rental abodes with the swipe of a finger</Text>
-        <Button onPress={ ()=>Actions.login({ data:"Custom data", title:"Custom title" })}> Login </Button>
-        <Button onPress={Actions.signup}> New Member? </Button>
       </View>
-    )
+    );
   }
 }
 
+// Original Version
+// class Intro extends React.Component {
+//   render() {
+//     var Actions = this.props.routes;      // console.log this.props.routes (what data/routes is inside?)
+    
+//     return (
+//       <View style={styles.container}>
+//         <Text>Welcome to Padticular!</Text>
+//         <Text>Discovering beautiful rental abodes with the swipe of a finger</Text>
+//         <Button onPress={ ()=>Actions.login({ data:"Custom data", title:"Custom title" })}> Login </Button>
+//         <Button onPress={Actions.signup}> New Member? </Button>
+//       </View>
+//     )
+//   }
+// }
+
 // Styling for Intro component page
-var styles = Stylesheet.create({
-  container: {
-    flex: 1, 
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'transparent',
-  },
-});
+// var styles = Stylesheet.create({
+//   container: {
+//     flex: 1, 
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: 'transparent',
+//   }
+// });
 
 module.exports = Intro;
--- index.ios.js
+++ index.ios.js
@@ -2,24 +2,24 @@
 // Pavel Aksonov's React-Native-Redux-Router sample code: https://github.com/aksonov/react-native-redux-router
 
 'use strict'
-const React = require('react-native');
-var { AppRegistry, StyleSheet, Text, View } = React;
-var { Animations, Container, Route, Router, routerReducer, Schema } = require('react-native-redux-router');
+import React, {Component} from 'react';
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+import { Animations, Container, Route, Router, routerReducer, Schema } from 'react-native-redux-router';
 
 // Padticular 2 components
-var Error     = require('./components/Error');
-var Homepage  = require('./components/Homepage');
+// var Error     = require('./components/Error');
+// var Homepage  = require('./components/Homepage');
 var Intro     = require('./components/Intro');
-var Login     = require('./components/Login');
-var Profile   = require('./components/Profile');
-var Search    = require('./components/Search');
-var Signup    = require('./components/Signup');
-var YesOrNo   = require('./components/YesOrNo');
-var { NavBar, NavBarModal } = require('./components/NavBar');
+// var Login     = require('./components/Login');
+// var Profile   = require('./components/Profile');
+// var Search    = require('./components/Search');
+// var Signup    = require('./components/Signup');
+// var YesOrNo   = require('./components/YesOrNo');
+// var { NavBar, NavBarModal } = require('./components/NavBar');
 
 // Redux modules 
 import { createStore, combineReducers, applyMiddleware } from 'redux';
-import Provider from 'react-redux';
+import { Provider } from 'react-redux';
 import createLogger from 'redux-logger';
 
 const loggerMiddleWare = createLogger();
@@ -27,8 +27,9 @@ const createStoreWithMiddleware = applyMiddleware(loggerMiddleWare)(createStore)
 const reducer = combineReducers({routerReducer});
 let store = createStoreWithMiddleware(reducer);
 
+
 // Initialization of all Padticular2 routes
-class App extends React.Component {
+class App extends Component {
   render() {
     return (
       <View style={{ flex:1 }}>
@@ -40,28 +41,47 @@ class App extends React.Component {
           <Schema name="tab" navBar={NavBar} />
 
           <Route name="intro" component={Intro} initial={true} hideNavBar={true} title="Launch" />
-          <Route name="signup" component={Signup} title="New User Registration" schema="modal" />
-          <Route name="login" component={Login} title="Log In Page" schema="modal" />
-          <Route name="homepage" component={Homepage} title="User Homepage" type="replace" />
-          <Route name="search" component={Search} title="Padticular Search" schema="modal" />
-          <Route name="yesorno" component={YesOrNo} title="YesOrNo" schema="noAnimation" />
-          <Route name="profile" component={Profile} title="Apartment Profile" schema="noAnimation" />
-          <Route name="error" component={Error} schema="popup" />
         </Router>
       </View>
-    )
+    );
   }
 }
 
-class Padticular extends React.Component {
+// Original Version
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <View style={{ flex:1 }}>
+//         <View style={{ position:'absolute', left:0, right:0, top:0, bottom:0, backgroundColor:'#F5FCFF' }} />
+//         <Router>
+//           <Schema name="modal" sceneConfig={Animations.FlatFloatFromBottom} navBar={NavBarModal} />
+//           <Schema name="default" sceneConfig={Animations.FlatFloatFromRight} navBar={NavBar} />
+//           <Schema name="noAnimation" navBar={NavBar} />
+//           <Schema name="tab" navBar={NavBar} />
+
+//           <Route name="intro" component={Intro} initial={true} hideNavBar={true} title="Launch" />
+//           <Route name="signup" component={Signup} title="New User Registration" schema="modal" />
+//           <Route name="login" component={Login} title="Log In Page" schema="modal" />
+//           <Route name="homepage" component={Homepage} title="User Homepage" type="replace" />
+//           <Route name="search" component={Search} title="Padticular Search" schema="modal" />
+//           <Route name="yesorno" component={YesOrNo} title="YesOrNo" schema="noAnimation" />
+//           <Route name="profile" component={Profile} title="Apartment Profile" schema="noAnimation" />
+//           <Route name="error" component={Error} schema="popup" />
+//         </Router>
+//       </View>
+//     )
+//   }
+// }
+
+class Padticular2 extends Component {
   render() {
     return (
       <Provider store={store}>
         {() => <App />}
       </Provider>
-    )
+    );
   }
 }
 
-AppRegistry.registerComponent('Padticular', () => Padticular);
+AppRegistry.registerComponent('Padticular2', () => Padticular2);
 
